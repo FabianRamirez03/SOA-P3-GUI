257f83248708ce47ef1ef1608b2e5037652df5b5
Mario Araya <marioaxtrem@gmail.com>
2022-11-15 10:50:35 -0600

    scripts y modificaciones al main

--- db-scripts/procedures script.sql
+++ db-scripts/procedures script.sql
@@ -1,13 +1,13 @@
 #agregar datos de gastos
 DELIMITER $$
 CREATE PROCEDURE agregarGastos(
-montoP decimal(15,3), 
+montoP varchar(15), 
 descripcionP varchar(50), 
 responsableP varchar(50),
 departamentoP varchar(50)
 )
 Begin
-	insert into gastos (monto, descripcion, responsable, departamento, fecha) values (montoP, descripcionP, responsableP, departamentoP, curdate());
+	insert into gastos (monto, descripcion, responsable, departamento, fecha) values (CAST(montoP AS DECIMAL(15,3)), descripcionP, responsableP, departamentoP, curdate());
 end 
 $$
 
@@ -35,9 +35,36 @@ select departamento, sum(monto) from gastos group by departamento order by sum(m
 END
 $$
 
-#call agregarGastos(500000, "compra de osciloscopio","Juan Jose", "Electronica")
-#select * from gastos
-#update gastos set fecha = '2022-9-9' where departamento = "Electronica";
+DELIMITER $$
+CREATE PROCEDURE verTodo ()
+BEGIN
+select * from gastos;
+END
+$$
+
+DELIMITER $$
+CREATE PROCEDURE getReportYears ()
+BEGIN
+select Year(fecha) from gastos group by year(fecha) order by year(fecha) asc;
+END
+$$
+
+#call verTodo()
+#call getReportYears()
+#call agregarGastos("10101", "libros","carlos", "matematica")
+#call agregarGastos("12312", "material laboratorio","miguel", "fisica")
+#call agregarGastos("23245", "fotocopias examenes","mariana", "fisica")
+#call agregarGastos("5673", "bolas de ping pong","beto", "deportiva")
+#call agregarGastos("12000", "redes de baloncesto","fabian", "deportiva")
+#call agregarGastos("5435346", "equipo de laboratorio","tomas", "electronica")
+#call agregarGastos("12443523", "carro nuevo","wajib", "electronica")
+
+
+#update gastos set fecha = '2021-10-10' where departamento = "deportiva";
+#update gastos set fecha = '2022-9-9' where departamento = "electronica";
+#update gastos set fecha = '2022-10-10' where departamento = "matematica";
+#update gastos set fecha = '2022-11-11' where departamento = "fisica";
+
 #call gastosMes(11,2022)
 #call totalMes(11,2022)
 #call masGastosDep()
